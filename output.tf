4b9fd247cf84e6070e919b7017863391cf9f233b
chule <lcasaretto@vi-datec.com>
2019-07-28 19:17:20 -0300

    Added outputs.tf file

---
+++ output.tf
@@ -0,0 +1,15 @@
+# outputs
+output "instances_ip" {
+  description = "EC2 IP 1"
+  value = "${aws_instance.chl1.public_ip}"
+}
+
+output "instance_ip" {
+  description = "EC2 IP 2"
+  value = "${aws_instance.chl2.public_ip}"
+}
+
+output "rds_ip" {
+  description = "RDS IP"
+  value = "${aws_db_instance.mariadb.endpoint}"
+}
